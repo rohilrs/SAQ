7e8eb0cc8518be574ef40238d14f9ac106c2d61a
Rohil Shah <rohilrshah@gmail.com>
2026-02-15 14:10:41 -0800

    feat: rewrite CAQ encoder + FastScan SIMD matching reference
    Port CaqCode, QuantBaseCode, CAQEncoder (encode, code_adjustment,
    encode_and_fac) from reference caq_encoder.hpp. Port fastscan.hpp
    and fastscan_highacc.hpp into unified fast_scan.h with pack_codes,
    accumulate, accumulate_hacc, transfer_lut_hacc, pack_lut.

---
+++ include/saq/caq_encoder.h
@@ -0,0 +1,247 @@
+#pragma once
+
+#include <cassert>
+#include <cmath>
+#include <cstdint>
+
+#include <glog/logging.h>
+
+#include "saq/defines.h"
+#include "saq/config.h"
+
+namespace saq {
+
+class CaqCode {
+  public:
+    double v_mx;  // max absolute value of the vector, v_mx = max{|o_i|}
+    double v_mi;  // v_mi = -v_mx
+    double delta; // (v_mx-v_mi) / 2^b
+    Eigen::VectorXi code;
+    double oa_l2sqr; // |o_a|^2, squared L2 norm of the quantized vector
+    double ip_o_oa;  // <o, o_a>
+
+    double o_l2sqr;     // |o|^2, squared L2 norm of the original vector
+    double o_l2norm;    // |o|, L2 norm of the original vector
+    double fac_rescale; // |o|^2 / <o, o_a>
+    double fac_error;   // |o|^2 * epsilon * sqrt((1 - <o, o_a>^2) / <o, o_a>^2) / sqrt(dim - 1)
+
+    void rescale_vmx_to1() {
+        if (!v_mx)
+            return;
+        const auto scale_rate = 1.0 / v_mx;
+        delta *= scale_rate;
+        ip_o_oa *= scale_rate;
+        oa_l2sqr *= scale_rate * scale_rate;
+        fac_rescale *= v_mx;
+
+        v_mi *= scale_rate;
+        v_mx *= scale_rate;
+    }
+
+    Eigen::VectorXf get_oa() const {
+        CHECK(code.size() >= 1) << "CaqCode::get_oa() called before encode.";
+        return (code.cast<float>() * delta).array() + v_mi;
+    }
+};
+
+class QuantBaseCode {
+  public:
+    Eigen::VectorXi code;
+    float o_l2norm;
+    float ip_cent_oa = 0;
+    float fac_rescale;      // rescale factor for estimation
+    float fac_error = 0;    // error factor for estimation
+    float norm_ip_o_oa = 0; // <o, o_a> / |o| / |o_a|. Only used for quant metrics
+};
+
+class CAQEncoder {
+    static constexpr float kConstEpsilon = 1.9;
+
+    const size_t num_dim_pad_;     // number of dimensions (padded)
+    const size_t num_bits_;        // Number of bits for quantization
+    const QuantSingleConfig &cfg_; // Quantization configuration
+    uint16_t code_max_;
+    const uint16_t code_mi_ = 0;
+
+    void code_adjustment(const float *curr_vec, CaqCode &caq) {
+        const auto &v_mi = caq.v_mi;
+        const auto &delta = caq.delta;
+        auto &code = caq.code;
+        double &ip_resi_oa = caq.ip_o_oa;
+        double &oa_l2sqr = caq.oa_l2sqr;
+
+        const double re_eps = cfg_.caq_adj_eps * oa_l2sqr;
+        [[maybe_unused]] int tot_adj_cnt = 0;
+        for (int adj_cnt = 1, round = 1; adj_cnt; round++) {
+            CHECK_GE(ip_resi_oa, 0);
+            adj_cnt = 0;
+            for (size_t j = 0; j < num_dim_pad_; j++) {
+                int curr_adj_cnt = 0;
+                const auto o = curr_vec[j];
+                double oa = (code[j] + 0.5) * delta + v_mi;
+                uint16_t c = code[j];
+                const auto oa_l2sqr_tmp = oa_l2sqr - oa * oa;
+                const auto ip_delta = delta * o;
+
+                // try ++
+                while (c < code_max_) {
+                    auto new_q = oa + delta;
+                    auto new_length = oa_l2sqr_tmp + new_q * new_q;
+                    auto new_ip = ip_resi_oa + ip_delta;
+                    if ((ip_resi_oa * ip_resi_oa + re_eps) * new_length >= new_ip * new_ip * oa_l2sqr)
+                        break;
+                    c++;
+                    ip_resi_oa = new_ip;
+                    oa = new_q;
+                    oa_l2sqr = new_length;
+
+                    curr_adj_cnt++;
+                }
+                // try --
+                while (c > code_mi_) {
+                    auto new_q = oa - delta;
+                    auto new_length = oa_l2sqr_tmp + new_q * new_q;
+                    auto new_ip = ip_resi_oa - ip_delta;
+                    if ((ip_resi_oa * ip_resi_oa + re_eps) * new_length >= new_ip * new_ip * oa_l2sqr)
+                        break;
+                    c--;
+                    ip_resi_oa = new_ip;
+                    oa = new_q;
+                    oa_l2sqr = new_length;
+
+                    curr_adj_cnt++;
+                }
+                if (code[j] != c) {
+                    code[j] = c;
+                    adj_cnt += curr_adj_cnt;
+                }
+            }
+            tot_adj_cnt += adj_cnt;
+            if (cfg_.caq_adj_rd_lmt && round >= cfg_.caq_adj_rd_lmt) {
+                break;
+            }
+
+            // correction
+            {
+                double check_oa_l2sqr = 0;
+                double check_ip = 0;
+                for (size_t j = 0; j < num_dim_pad_; ++j) {
+                    const auto o = curr_vec[j];
+                    auto qc = code[j];
+                    auto q = (qc + 0.5) * delta + v_mi;
+                    check_ip += q * o;
+                    check_oa_l2sqr += q * q;
+                }
+                oa_l2sqr = check_oa_l2sqr;
+                ip_resi_oa = check_ip;
+            }
+        }
+    }
+
+    void downUpSample(const float *vec, CaqCode &caq) {
+        auto sampled_rshirt = cfg_.caq_ori_qB - num_bits_;
+        caq.delta *= 1 << sampled_rshirt;
+        caq.ip_o_oa = 0;
+        caq.oa_l2sqr = 0;
+        for (size_t j = 0; j < num_dim_pad_; ++j) {
+            caq.code[j] >>= sampled_rshirt;
+            const auto o = vec[j];
+            auto qc = caq.code[j];
+            auto q = (qc + 0.5) * caq.delta + caq.v_mi;
+            caq.ip_o_oa += q * o;
+            caq.oa_l2sqr += q * q;
+        }
+    }
+
+  public:
+    CAQEncoder(size_t num_dim_pad, size_t num_bits, const QuantSingleConfig &cfg)
+        : num_dim_pad_(num_dim_pad), num_bits_(num_bits), cfg_(cfg),
+          code_max_((1 << num_bits_) - 1) {
+        CHECK_LE(num_bits_, KMaxQuantizeBits) << "CAQ only support up to " << KMaxQuantizeBits << " bits";
+
+        if (cfg_.caq_ori_qB) {
+            CHECK_GT(cfg_.caq_ori_qB, static_cast<int>(num_bits_));
+            code_max_ = (1 << cfg_.caq_ori_qB) - 1;
+            CHECK_NE(cfg_.caq_ori_qB, static_cast<int>(num_bits_));
+        }
+    }
+
+    void encode(const FloatVec &o, CaqCode &caq) {
+        if (num_bits_ == 0) {
+            caq = CaqCode();
+            return;
+        }
+        auto &code = caq.code;
+        auto &v_mi = caq.v_mi;
+        auto &v_mx = caq.v_mx;
+        auto &delta = caq.delta;
+        auto &ip_o_oa = caq.ip_o_oa;
+        auto &oa_l2sqr = caq.oa_l2sqr;
+
+        v_mx = std::max(o.maxCoeff(), -o.minCoeff());
+        v_mi = -v_mx;
+
+        delta = (v_mx - v_mi) / (code_max_ + 1);
+        double vec_sum = o.sum();
+
+        Eigen::VectorXf code_f;
+        if (delta) {
+            code_f = ((o.array() - v_mi) / delta).floor().array().min(code_max_);
+        } else {
+            code_f = Eigen::VectorXf::Zero(num_dim_pad_);
+        }
+
+        double ip_o_code = 0;
+        uint64_t code_l2sqr = 0;
+        code.resize(num_dim_pad_);
+        for (size_t j = 0; j < num_dim_pad_; j++) {
+            ip_o_code += code_f[j] * o[j];
+            int c = static_cast<int>(code_f[j]);
+            code[j] = c;
+            code_l2sqr += c * c;
+        }
+        const uint32_t code_sum = code.sum();
+
+        ip_o_oa = ip_o_code * delta + (v_mi + 0.5 * delta) * vec_sum;
+        oa_l2sqr = delta * delta * code_l2sqr + (delta * delta + 2 * delta * v_mi) * code_sum;
+        oa_l2sqr += (0.25 * delta * delta + delta * v_mi + v_mi * v_mi) * num_dim_pad_;
+
+        if (cfg_.caq_adj_rd_lmt && oa_l2sqr) {
+            DCHECK(delta);
+            code_adjustment(o.data(), caq);
+        }
+
+        if (cfg_.caq_ori_qB) {
+            downUpSample(o.data(), caq);
+        }
+    }
+
+    void encode_and_fac(const FloatVec &curr_vec, QuantBaseCode &base_code, const FloatVec *centroid) {
+        CaqCode caq;
+        encode(curr_vec, caq);
+        caq.o_l2sqr = curr_vec.squaredNorm();
+        caq.o_l2norm = std::sqrt(caq.o_l2sqr);
+        caq.fac_rescale = caq.ip_o_oa ? caq.o_l2sqr / caq.ip_o_oa : 0;
+
+        // err = o_l2sqr * epsilon * sqrt((1 - <o, o_a>^2) / <o, o_a>^2) / sqrt(dim - 1)
+        caq.fac_error = caq.o_l2sqr * kConstEpsilon *
+                        std::sqrt((((caq.o_l2sqr * caq.oa_l2sqr) / (caq.ip_o_oa * caq.ip_o_oa)) - 1) /
+                                  (num_dim_pad_ - 1));
+
+        caq.rescale_vmx_to1();
+
+        base_code.o_l2norm = caq.o_l2norm;
+        base_code.fac_rescale = caq.fac_rescale;
+        base_code.fac_error = caq.fac_error;
+        if (num_bits_ > 1) {
+            if (centroid) {
+                base_code.ip_cent_oa = centroid->dot(caq.get_oa());
+            }
+            base_code.norm_ip_o_oa = caq.ip_o_oa / caq.o_l2norm / std::sqrt(caq.oa_l2sqr);
+
+            base_code.code = std::move(caq.code);
+        }
+    }
+};
+
+} // namespace saq
---
+++ src/caq_encoder.cpp
@@ -0,0 +1,8 @@
+/// @file caq_encoder.cpp
+/// @brief CAQ (Code Adjustment Quantization) encoder implementation.
+///
+/// All methods are defined inline in the header (caq_encoder.h).
+/// This translation unit ensures the header compiles cleanly and
+/// provides a place for any future non-inline implementations.
+
+#include "saq/caq_encoder.h"
